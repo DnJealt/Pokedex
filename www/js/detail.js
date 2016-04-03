09cc0862139841eea6b9614fb79c53c357585bc1
joostvanrijsinge <jjrijsin@student.avans.nl>
2016-04-04 01:08:17 +0200

    DETAILS with img charmander so cute <3

--- www/js/detail.js
+++ www/js/detail.js
@@ -1,13 +1,35 @@
 var utility = new JSONUtility();
+var selectedPokemon = utility.getJSONData('selectedPokemon');
 
 setHeader();
 loadPokemonData();
 
 function setHeader() {
-    var selectedPokemon = utility.getJSONData('selectedPokemon');
     $('#pokemon-name').append(selectedPokemon['pokemon_species']['name']);
 }
 
 function loadPokemonData(){
-    // Your turn Joost.
+    var thisUrl = 'http://www.pokeapi.co/api/v2/pokemon/' + selectedPokemon['entry_number'];
+    
+    $.ajax({
+        url: thisUrl,
+        type: 'GET',
+        success: function(data){
+            var parsedData = utility.parseData(data);
+            console.log(parsedData);
+            populateDetails(parsedData);     
+    
+                           
+            },
+            error: function(err){
+                console.log(err);
+            //  $.mobile.loading('hide');  
+            }                   
+    });
+}
+
+function populateDetails(data) {
+    $('#detailImg').append("<img src='" + data['sprites']['front_default'] + "'/>");
+    $('#detailHeight').append(data['height']);
+    $('#detailWeight').append(data['weight']);
 }
