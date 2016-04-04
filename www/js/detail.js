b18d75e5eb19aeafa33f52d559c01b374ce95f10
joostvanrijsinge <jjrijsin@student.avans.nl>
2016-04-04 12:22:48 +0200

    more details

--- www/js/detail.js
+++ www/js/detail.js
@@ -1,6 +1,7 @@
 var utility = new JSONUtility();
 var selectedPokemon = utility.getJSONData('selectedPokemon');
 
+$.mobile.loading('show');
 setHeader();
 loadPokemonData();
 
@@ -8,38 +9,37 @@ function setHeader() {
     $('#pokemon-name').append(selectedPokemon['pokemon_species']['name']);
 }
 
-function loadPokemonData(){
+function loadPokemonData() {
     var thisUrl = 'http://www.pokeapi.co/api/v2/pokemon/' + selectedPokemon['entry_number'];
-    $.mobile.loading('show');
     $.ajax({
         url: thisUrl,
         type: 'GET',
-        success: function(data){
+        success: function(data) {
             var parsedData = utility.parseData(data);
             console.log(parsedData);
             populateDetails(parsedData);
-            $.mobile.loading('hide'); 
-    
-                           
-            },
-            error: function(err){
-                console.log(err);
-             $.mobile.loading('hide');  
-            }                   
+
+        },
+        error: function(err) {
+            console.log(err);
+            $.mobile.loading('hide');
+        }
     });
 }
 
 function populateDetails(data) {
     $('#detailImg').append("<img src='" + data['sprites']['front_default'] + "'/>");
-    $('#detailHeight').append((data['height']/10) + "m");
-    $('#detailWeight').append((data['weight']/10) + "kg");
-    
+    $('#detailHeight').append((data['height'] / 10) + "m");
+    $('#detailWeight').append((data['weight'] / 10) + "kg");
+
     if (data['types'][1] != null) {
         $('#detailType1').append(data['types'][1]['type']['name']);
         $('#detailType2').append(data['types'][0]['type']['name']);
     } else {
         $('#detailType1').append(data['types'][0]['type']['name']);
     }
-    
-    
+
+
+
+    $.mobile.loading('hide');
 }
